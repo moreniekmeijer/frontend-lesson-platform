6315d0f193d81e014262ba15060836a542abc3d2
moreniekmeijer <niekjmeijer@gmail.com>
2025-02-28 17:43:14 +0100

    Started on materialForm component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Button from "./components/button/Button.jsx";
 import StyleTile from "./components/styleTile/StyleTile.jsx";
 import Aside from "./components/aside/Aside.jsx";
 import DragDrop from "./components/dragDrop/DragDrop.jsx";
+import { useForm } from 'react-hook-form';
+import MaterialForm from "./components/materialForm/materialForm.jsx";
 
 function App() {
 
@@ -15,7 +17,13 @@ function App() {
             <main>
                 <DragDrop></DragDrop>
                 <StyleTile></StyleTile>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolorem ducimus iusto laudantium, magnam necessitatibus quis vel! Ab atque dicta excepturi odio porro quisquam quos repellendus sit tempore voluptatibus? Non? loerm   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ea et eum maxime, officia quae ratione repellendus repudiandae! Architecto cum deserunt facilis id laboriosam magni, mollitia numquam quod ratione tempora.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolorem ducimus iusto laudantium,
+                magnam necessitatibus quis vel! Ab atque dicta excepturi odio porro quisquam quos repellendus sit
+                tempore voluptatibus? Non? loerm Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ea
+                et eum maxime, officia quae ratione repellendus repudiandae! Architecto cum deserunt facilis id
+                laboriosam magni, mollitia numquam quod ratione tempora.
+
+                <MaterialForm></MaterialForm>
             </main>
             <Aside></Aside>
         </>
---
+++ src/components/materialForm/materialForm.jsx
@@ -0,0 +1,99 @@
+import {useForm} from "react-hook-form";
+import DragDrop from "../dragDrop/DragDrop.jsx";
+import "./materialForm.css"
+import {useState} from "react";
+
+function MaterialForm(props) {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [file, setFile] = useState(null);
+
+    function handleFileSelect(selectedFile) {
+        setFile(selectedFile);
+    }
+
+    function handleFormSubmit(data) {
+        const formData = new FormData();
+
+        // Voeg de tekstvelden toe
+        Object.keys(data).forEach((key) => {
+            formData.append(key, data[key]);
+        });
+
+        // Voeg het bestand toe als het bestaat
+        if (file) {
+            formData.append("file", file);
+        }
+
+        // Debugging: Log wat er wordt verstuurd
+        for (let pair of formData.entries()) {
+            console.log(pair[0], pair[1]);
+        }
+
+        console.log(formData);
+    }
+
+    return (
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+
+            <label>Upload een bestand:</label>
+            <DragDrop onFileSelect={handleFileSelect}/>
+
+            <label htmlFor="title">
+                Titel:
+                <input
+                    type="text"
+                    id="title"
+                    {...register("title", {
+                        required: {
+                            value: true,
+                            message: "Titel is verplicht"
+                        }
+                    })}
+                />
+            </label>
+            {errors.title && <p>{errors.title.message}</p>}
+
+            <label htmlFor="link">
+                Link (optioneel):
+                <input
+                    type="text"
+                    id="link"
+                    {...register("link", {
+                        pattern: {
+                            value: /^(https?:\/\/)?([\w-]+(\.[\w-]+)+)(\/.*)?$/,
+                            message: "Moet een geldige link zijn (bijv. https://example.com)",
+                        },
+                    })}
+                    placeholder="https://example.com"
+                />
+            </label>
+            {errors.link && <p>{errors.link.message}</p>}
+
+            <label htmlFor="type">
+                Type materiaal (optioneel):
+                <input
+                    type="text"
+                    id="type"
+                    placeholder="Bijv: break of solo"
+                    {...register("type")}
+                />
+            </label>
+
+            <label htmlFor="instrument">
+                Instrument (optioneel):
+                <input
+                    type="instrument"
+                    id="instrument"
+                    placeholder="Bijv: djembé"
+                    {...register("instrument")}
+                />
+            </label>
+
+            <button type="submit">
+                Opslaan
+            </button>
+        </form>
+    )
+}
+
+export default MaterialForm;
