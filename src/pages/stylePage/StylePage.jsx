c1f235bd960eec44a03ddbec9f4c9379a5e52454
moreniekmeijer <niekjmeijer@gmail.com>
2025-03-18 11:30:33 +0100

    Updated StylePage

--- src/pages/stylePage/StylePage.jsx
+++ src/pages/stylePage/StylePage.jsx
@@ -1,12 +1,23 @@
 import StyleTile from "../../components/styleTile/StyleTile.jsx";
 import MoreItemsTile from "../../components/moreVideosTile/MoreItemsTile.jsx";
 import CountryTile from "../../components/countryTile/CountryTile.jsx";
+import "./StylePage.css"
 
 function StylePage() {
+    // Alles moet hier opgehaald worden via de GET styles, via een id parameter. (dus niet in de styleTile)
+
+    const videoExamples = [
+        { type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s" },
+        { type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s" },
+        { type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s" },
+        { type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s" },
+        { type: "video", url: "https://www.youtube.com/watch?v=abc123" }
+    ];
+
     return (
         <section className="style-page">
             <StyleTile/>
-            <MoreItemsTile/>
+            <MoreItemsTile title="Videos" items={videoExamples}/>
             <CountryTile countryName="nederland"/>
         </section>
     )
