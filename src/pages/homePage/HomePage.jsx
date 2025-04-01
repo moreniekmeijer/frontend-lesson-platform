252ab08630193fa6cb46661d3c471ee246907c49
moreniekmeijer <niekjmeijer@gmail.com>
2025-04-01 10:55:51 +0200

    Updated main layout

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -3,17 +3,18 @@ import axios from "axios";
 import MoreItemsTile from "../../components/moreVideosTile/MoreItemsTile.jsx";
 import styles from "./HomePage.module.css"
 import SearchTile from "../../components/searchTile/SearchTile.jsx";
+import "../../App.css"
 
 function HomePage() {
     const [lesson, setLesson] = useState([]);
     const [lessonStyles, setLessonStyles] = useState({});
 
     const videoExamples = [
-        { type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s" },
-        { type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s" },
-        { type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s" },
-        { type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s" },
-        { type: "video", url: "https://www.youtube.com/watch?v=abc123" }
+        {type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s"},
+        {type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s"},
+        {type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s"},
+        {type: "video", url: "https://www.youtube.com/watch?v=DqZX8m2T-e0&t=193s"},
+        {type: "video", url: "https://www.youtube.com/watch?v=abc123"}
     ];
 
     async function getLessonData() {
@@ -32,24 +33,32 @@ function HomePage() {
 
     return (
         <>
-            <section className={styles.lessons}>
-                <MoreItemsTile title="Volgende les" items={videoExamples} />
-                <section className={styles.notes}>
-                    <h3>Notities</h3>
-                    <p>{lesson.notes}</p>
-                </section>
-                <section className={styles.agenda}>
-                    <h3>Agenda</h3>
-                    <ul>
-                        <li>wordt denk ik eigen component</li>
-                        <li>of {lesson.scheduledDateTime}</li>
-                        <li>01-01-2025</li>
-                    </ul>
-                </section>
-            </section>
-            <MoreItemsTile title="Nieuw toegevoegd" items={videoExamples}/>
-            <MoreItemsTile title="Videos volgende les" items={videoExamples}/>
-            <SearchTile/>
+            <div className="upperContainer">
+                <div className="leftItems">
+                    <MoreItemsTile title="Volgende les" items={videoExamples}/>
+                </div>
+                <div>
+                    <section className={styles.notes}>
+                        <h3>Notities</h3>
+                        <p>{lesson.notes}</p>
+                    </section>
+                    <section className={styles.agenda}>
+                        <h3>Agenda</h3>
+                        <ul>
+                            <li>wordt denk ik eigen component</li>
+                            <li>of {lesson.scheduledDateTime}</li>
+                            <li>01-01-2025</li>
+                        </ul>
+                    </section>
+                </div>
+            </div>
+            <div className="lowerContainer">
+                <div className="leftItems">
+                    <MoreItemsTile title="Nieuw toegevoegd" items={videoExamples}/>
+                    <MoreItemsTile title="Videos volgende les" items={videoExamples}/>
+                </div>
+                <SearchTile/>
+            </div>
         </>
     )
 }
