63ade55d4618df945ea7127efa586368b58624c4
moreniekmeijer <niekjmeijer@gmail.com>
2025-04-02 22:58:39 +0200

    Updated StyleTile

--- src/components/styleTile/StyleTile.jsx
+++ src/components/styleTile/StyleTile.jsx
@@ -1,7 +1,7 @@
 import styles from "./StyleTile.module.css";
 import Button from "../button/Button.jsx";
 
-function StyleTile({ data }) {
+function StyleTile({data}) {
     if (!data) return null;
 
     return (
@@ -11,10 +11,19 @@ function StyleTile({ data }) {
                 <Button>Arrangement</Button>
             </span>
             <p>{data.description}</p>
+            {/*TODO - de backend geeft nu alleen links mee vanuit materials (in get /styles), dus er is geen metadata, dus ik kan de links*/}
+            {/*     - geen titel geven vanuit de backend... Misschien toch de volledige material objecten meesturen?*/}
+            {/*TODO - Misschien ook de 'geen links beschikbaar' fallback weghalen, want is niet nodig*/}
             <ul>
-                {data.links?.map((link, index) => (
-                    <li key={index}><a href={link.url}>{link.title}</a></li>
-                )) || <li>Geen links beschikbaar</li>}
+                {data.links && data.links.length > 0 ? (
+                    data.links.map((link, index) => (
+                        <li key={index}>
+                            <a href={link} target="_blank" rel="noopener noreferrer">{link}</a>
+                        </li>
+                    ))
+                ) : (
+                    <li>Geen links beschikbaar</li>
+                )}
             </ul>
         </article>
     );
