85af6d413fc7b0d25390bccac9705f681c42fe2a
moreniekmeijer <niekjmeijer@gmail.com>
2025-04-06 21:13:53 +0200

    Updated AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,33 +1,96 @@
-import React, { createContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {createContext, useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {jwtDecode} from "jwt-decode";
+import axios from "axios";
 
 export const AuthContext = createContext({});
 
-function AuthContextProvider({ children }) {
-    const [isAuth, toggleIsAuth] = useState(false);
+function AuthContextProvider({children}) {
+    const [auth, setAuth] = useState({
+        isAuth: false,
+        user: null,
+        status: 'pending',
+    });
     const navigate = useNavigate();
 
-    function login() {
-        console.log('Gebruiker is ingelogd!');
-        toggleIsAuth(true);
-        navigate('/');
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            const decoded = jwtDecode(token);
+            void fetchUserData(decoded.sub, token);
+        } else {
+            setAuth({
+                isAuth: false,
+                user: null,
+                status: 'done',
+            });
+        }
+    }, []);
+
+    function login(token) {
+        localStorage.setItem('token', token);
+        const decoded = jwtDecode(token);
+
+        void fetchUserData(decoded.sub, token, '/profile');
+
     }
 
     function logout() {
+        localStorage.clear();
+        setAuth({
+            isAuth: false,
+            user: null,
+            status: 'done',
+        });
+
         console.log('Gebruiker is uitgelogd!');
-        toggleIsAuth(false);
         navigate('/');
     }
 
+    async function fetchUserData(id, token, redirectUrl) {
+        try {
+            const result = await axios.get(`http://localhost:3000/600/users/${id}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            setAuth({
+                ...auth,
+                isAuth: true,
+                user: {
+                    username: result.data.username,
+                    email: result.data.email,
+                    id: result.data.id,
+                },
+                status: 'done',
+            });
+
+            if (redirectUrl) {
+                navigate(redirectUrl);
+            }
+
+        } catch (e) {
+            console.error(e);
+            setAuth({
+                isAuth: false,
+                user: null,
+                status: 'done',
+            });
+        }
+    }
+
     const contextData = {
-        isAuth: isAuth,
-        login: login,
-        logout: logout,
+        ...auth,
+        login,
+        logout
     };
 
     return (
         <AuthContext.Provider value={contextData}>
-            {children}
+            {auth.status === 'done' ? children : <p>Loading...</p>}
         </AuthContext.Provider>
     );
 }
