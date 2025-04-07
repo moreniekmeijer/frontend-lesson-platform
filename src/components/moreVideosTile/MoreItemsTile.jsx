9ecc63d818a67bbf0cb36c480466b24acff8a887
moreniekmeijer <niekjmeijer@gmail.com>
2025-04-07 23:11:01 +0200

    Updated MoreItemsTile

--- src/components/moreVideosTile/MoreItemsTile.jsx
+++ src/components/moreVideosTile/MoreItemsTile.jsx
@@ -10,17 +10,30 @@ function MoreItemsTile({title, items, variant}) {
             <ul className={styles.itemList}>
                 {items.map((item, index) => (
                     <li key={index}>
-                        {item.fileType === "VIDEO" && (
+
+                        {item.fileType === "VIDEO" && item.fileLink && (
                             <video width="200" height="157" controls>
-                                <source src={item.link} type="video/mp4"/>
+                                <source src={item.fileLink} type="video/mp4"/>
                                 Your browser does not support the video tag.
                             </video>
                         )}
-                        {item.fileType === "IMAGE" && (
-                            <img src={item.filePath} alt={`Item ${index}`} width="200" height="157"/>
+
+                        {item.fileType === "LINK" && item.fileLink && (
+                            <img
+                                src="../../assets/YT-logo.png"
+                                alt="Video Link"
+                                width="200"
+                                height="130"
+                            />
                         )}
-                        {item.fileType === "PDF" && (
-                            <p>{item.content}</p>
+
+                        {item.fileType === "PDF" && item.fileLink && (
+                            <img
+                                src="https://upload.wikimedia.org/wikipedia/commons/8/87/PDF_file_icon.svg"
+                                alt="PDF Preview"
+                                width="200"
+                                height="130"
+                            />
                         )}
                     </li>
                 ))}
--- src/pages/materialPage/MaterialPage.jsx
+++ src/pages/materialPage/MaterialPage.jsx
@@ -1,6 +1,12 @@
 function MaterialPage() {
     return (
-        <section className="material-page">Materials</section>
+        <section className="material-page">
+            <video controls width="600">
+                <source src="http://localhost:8080/materials/1/file" type="video/mp4"/>
+                Your browser does not support the video tag.
+            </video>
+        </section>
+
     )
 }
 
